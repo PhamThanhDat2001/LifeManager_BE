d59b876441ede30d4f211c4cd3313d6efc9834b0
datpt2 <phamthanhdat186@gmail.com>
2025-03-24 13:31:18 +0700

    Initial base

---
+++ src/test/java/com/project/lifemanager/LifemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.lifemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
